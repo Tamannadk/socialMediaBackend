a2c4ced1fbc337524c95a1711ca5246088163f78
Tamannadk <tamannadk4@gmail.com>
2024-09-25 14:12:42 +0530

    follow controllers started

---
+++ src/controllers/follow.controllers.js
@@ -0,0 +1,23 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+
+const followUser=asyncHandler(async(req,res)=>{
+
+})
+
+const unfollowUser=asyncHandler(async(req,res)=>{
+
+})
+
+const getFollowers=asyncHandler(async(req,res)=>{
+    //followers
+    
+})
+
+const getFollowing=asyncHandler(async(req,res)=>{
+
+})
+
+export {followUser,unfollowUser,getFollowers,getFollowing}
